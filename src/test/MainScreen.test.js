98138b7819873205dcc60667a52967f2b47ada90
Jaesik Kim <tensaijskim1991@gmail.com>
2021-04-17 00:08:55 +0900

    Adding beforeEach

--- src/test/MainScreen.test.js
+++ src/test/MainScreen.test.js
@@ -2,13 +2,19 @@ import {render} from '@testing-library/react';
 import MainScreen from "../components/MainScreen";
 
 describe('MainScreen Unit Test', () => {
-    test('main screen shows a book title', () => {
-        const stubBookRepository = jest.fn();
+
+    let stubBookRepository;
+
+    beforeEach(() => {
+        stubBookRepository = jest.fn();
+
+    })
+
+    test('shows a book title', () => {
         stubBookRepository.mockReturnValue([
             {
                 title: 'Book Title1'
-            }]
-        );
+            }]);
         const getBooks_return_value = stubBookRepository();
 
         const {getByText} = render(<MainScreen books={getBooks_return_value}/>);
@@ -16,13 +22,12 @@ describe('MainScreen Unit Test', () => {
         expect(getByText('Book Title1')).toBeInTheDocument();
     });
 
-    test('main screen shows nothing when empty', () => {
+    test('shows nothing when empty', () => {
         const {queryAllByText} = render(<MainScreen books={[]}/>);
         expect(queryAllByText('Book Title1').length).toBe(0);
     });
 
-    test('main screen shows multiple titles dynamically', () => {
-        const stubBookRepository = jest.fn();
+    test('ㅂshows multiple titles dynamically', () => {
         stubBookRepository.mockReturnValue([
             {
                 title: 'Book1'
